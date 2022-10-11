6b9cdbe28469abcde1a32df8f96fe68573dac26f
vinicius <v.nunhez27@gmail.com>
2022-10-11 15:05:02 -0300

    JS:Score Implementation

--- app.js
+++ app.js
@@ -1,30 +1,41 @@
 const form = document.querySelector('.quiz-form')
-const textScore = document.querySelector('.score')
+const finalResult = document.querySelector('result')
+const scoreText = document.querySelector('.score')
 
-const anserwsCorrect = ['C', 'A', 'B', 'C']
+const CorrectAnswers = ['C', 'A', 'B', 'C']
 
 form.addEventListener('submit', event => {
     event.preventDefault()
 
+ let score = 0
 
-let score = 0
-
- const userAnswers =  [
+ const answersUser =  [
    form.inputQuestion1.value,
    form.inputQuestion2.value,
    form.inputQuestion3.value,
    form.inputQuestion4.value,
  ]
 
- userAnswers.forEach((userAnswer, index) => {
-    if(userAnswer === anserwsCorrect[index]){
+ answersUser.forEach((userAnswer, index) => {
+    if(userAnswer === CorrectAnswers[index]){
       score += 25
     }
  })
 
-  textScore.innerHTML = `<h3>Seu score é: ${score}</h3`  
+ scrollTo(0,100)
+ finalResult.classList.remove('d-none')
 
+ let counter = 0
+
+ const timer = setInterval(() => {
+  if(counter === score){
+    clearInterval(timer)
+  } 
+
+  finalResult.querySelector('span').textContent = `${counter}%`
+   counter++
+ },10)
 
 })
 
-  
+
